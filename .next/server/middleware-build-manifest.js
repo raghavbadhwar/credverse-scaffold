c0d83dca734dce380d60ece494f5ff4d221fafe3
raghavbadhwar <raghavbadhwar7@gmail.com>
2025-08-23 02:24:45 +0530

    chore: add build artifacts

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,18 +1,1 @@
-self.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [],
-  "pages": {
-    "/_app": []
-  },
-  "ampFirstPages": []
-};
-self.__BUILD_MANIFEST.lowPriorityFiles = [
-"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
-];
+self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-8b4aa73748bfa88f.js","static/chunks/fd9d1056-8a61572848a53694.js","static/chunks/117-9a3b5ed5aec3430e.js","static/chunks/main-app-b3e8cc9d013301aa.js"],pages:{"/_app":["static/chunks/webpack-8b4aa73748bfa88f.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-30bd3d14d221007a.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-8b4aa73748bfa88f.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-30bd3d14d221007a.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
